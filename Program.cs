37b051a2202fdcfbff6b20d3156ad525fdf41831
shailijha <shaili_jha@yahoo.co.in>
2019-08-28 11:36:17 -0400

    Constructor code

--- Program.cs
+++ Program.cs
@@ -2,11 +2,26 @@
 
 namespace Legos
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      var dozer1 = new BullDozer("Blue", "Mini");
+      var dozer2 = new BullDozer("Red", "Large");
+      Console.WriteLine("Bulldozer1 is " + dozer1.color);
+      Console.WriteLine("Bulldozer2 is " + dozer2.color);
     }
+  }
+
+  public class BullDozer
+  {
+    public BullDozer(string _color, string _size)
+    {
+      this.color = _color;
+      this.size = _size;
+    }
+
+    public string color { get; set; }
+    public string size { get; set; }
+  }
 }
